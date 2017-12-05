1e7ad2a723a403c2714f3a5b01ca60849631defd
Sóti Csenge <soti.csenge@emarsys.com>
2017-12-05 11:39:25 +0100

    [rawSelectItSpec] add analyzeRawSelect it test

--- src/it/scala/com/emarsys/rdb/connector/redshift/RedshiftRawSelectItSpec.scala
+++ src/it/scala/com/emarsys/rdb/connector/redshift/RedshiftRawSelectItSpec.scala
@@ -29,4 +29,17 @@ class RedshiftRawSelectItSpec extends TestKit(ActorSystem()) with RawSelectItSpe
   override val badSimpleSelect = s"""SELECT * ForM "$aTableName""""
   override val simpleSelectNoSemicolon = s"""SELECT * FROM "$aTableName""""
 
+  "#analyzeRawSelect" should {
+    "return result" in {
+      val result = getStreamResult(connector.analyzeRawSelect(simpleSelect))
+
+      result shouldEqual Seq(
+        Seq("QUERY PLAN"),
+        Seq(s"""XN Seq Scan on "$aTableName"  (cost=0.00..0.07 rows=7 width=405)"""),
+        Seq(s"----- Tables missing statistics: $aTableName -----"),
+        Seq("----- Update statistics by running the ANALYZE command on these tables -----")
+      )
+    }
+  }
+
 }
