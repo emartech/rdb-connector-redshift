429ecf9c1175485ef1715c3ccecb19ee1da25545
ksisu <kristof.horvath@emarsys.com>
2018-03-06 14:57:21 +0100

    [simple_select] add optional distinct flag

--- src/it/scala/com/emarsys/rdb/connector/redshift/RedshiftSimpleSelectItSpec.scala
+++ src/it/scala/com/emarsys/rdb/connector/redshift/RedshiftSimpleSelectItSpec.scala
@@ -5,12 +5,14 @@ import akka.stream.{ActorMaterializer, Materializer}
 import akka.testkit.TestKit
 import com.emarsys.rdb.connector.common.models.SimpleSelect
 import com.emarsys.rdb.connector.common.models.SimpleSelect._
-import com.emarsys.rdb.connector.redshift.utils.SelectDbInitHelper
+import com.emarsys.rdb.connector.redshift.utils.{SelectDbInitHelper, TestHelper}
 import com.emarsys.rdb.connector.test.SimpleSelectItSpec
 
 import concurrent.duration._
+import scala.concurrent.Await
 
 class RedshiftSimpleSelectItSpec extends TestKit(ActorSystem()) with SimpleSelectItSpec with SelectDbInitHelper {
+  import scala.concurrent.ExecutionContext.Implicits.global
 
   override implicit val materializer: Materializer = ActorMaterializer()
 
@@ -20,6 +22,34 @@ class RedshiftSimpleSelectItSpec extends TestKit(ActorSystem()) with SimpleSelec
     system.terminate()
     super.afterAll()
   }
+
+  override def initDb(): Unit = {
+    super.initDb()
+
+    val createCTableSql =
+      s"""CREATE TABLE "$cTableName" (
+         |    C varchar(255) NOT NULL
+         |);""".stripMargin
+
+    val insertCDataSql =
+      s"""INSERT INTO "$cTableName" (C) VALUES
+         |('c12'),
+         |('c12'),
+         |('c3')
+         |;""".stripMargin
+
+    Await.result(for {
+      _ <- TestHelper.executeQuery(createCTableSql)
+      _ <- TestHelper.executeQuery(insertCDataSql)
+    } yield (), 5.seconds)
+  }
+
+  override def cleanUpDb(): Unit = {
+    val dropCTableSql = s"""DROP TABLE "$cTableName";"""
+    Await.result(TestHelper.executeQuery(dropCTableSql), 5.seconds)
+    super.cleanUpDb()
+  }
+
 
   "list table values with EQUAL" in {
     val simpleSelect = SimpleSelect(AllField, TableName(aTableName), where = Some(EqualToValue(FieldName("A2"), Value("3"))))
